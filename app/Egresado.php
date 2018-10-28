0276c4621ce244003482b607c4a21249334f2537
jjlondono <jjlondono@utp.edu.co>
2018-10-27 21:42:38 -0500

    creacion de entidades y relaciones, creacion de controladores y rutas

--- app/Egresado.php
+++ app/Egresado.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Egresado extends Model
 {
+
+	protected $table = 'egresados';
     //relacion
+    public function administrador(){
+    	return $this->belongsTo('App\Administrador', 'admin_id');
+    }
 }
---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function register(Request $request){
+    	echo "Accion registro"; die();
+    }
+
+    public function login(Request $request){
+    	echo "Accion login"; die();
+    }
+
+
+}
