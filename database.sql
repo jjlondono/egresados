93be2f18ee9d7e661723148f8370c7545c3643c6
jpav21 <jpav21@utp.edu.co>
2018-10-30 21:16:38 -0500

    modifcar el sql de la base de datos

--- database.sql
+++ database.sql
@@ -19,8 +19,7 @@ CONSTRAINT pk_users PRIMARY KEY(id)
 
 CREATE TABLE egresados(
 id 		int(255) auto_increment not null,
-user_id int(255) not null,
-nombre	varchar(255),
+name	varchar(255),
 pais varchar(255),
 dni	varchar(100),
 email  varchar(100),
@@ -31,7 +30,6 @@ genero varchar(255),
 created_at datetime DEFAULT NULL,
 updated_at datetime DEFAULT NULL,
 CONSTRAINT pk_cars PRIMARY KEY(id),
-CONSTRAINT fk_cars_users FOREIGN KEY(user_id) REFERENCES users(id)
 )ENGINE=InnoDb;CREATE DATABASE IF NOT EXISTS api;
 USE api;
 
@@ -47,17 +45,3 @@ remember_token varchar(255),
 CONSTRAINT pk_administrador PRIMARY KEY(dni)
 )ENGINE=InnoDb;
 
-CREATE TABLE egresado(
-dni 		int(255) not null,
-admin_id int(255) not null,
-nombre	varchar(255),
-pais varchar(100),
-email	varchar(30),
-password  varchar(255),
-intereses varchar(255),
-edad varchar(100),
-genero varchar(100),
-datos varchar(255),
-CONSTRAINT pk_egresado PRIMARY KEY(dni),
-CONSTRAINT fk_egresado_administrador FOREIGN KEY(admin_id) REFERENCES administrador(dni)
-)ENGINE=InnoDb;
